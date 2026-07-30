1ac2f834644cd189fc66f8339d7a09cee133cfd9
Lucimar Santos <lucimarcontas@gmail.com>
2026-07-30 17:03:54 -0300

    Exercicio Funções

--- aula.js
+++ aula.js
@@ -1,2 +1,13 @@
 const prompt = require (`prompt-sync`)();
-const nome = prompt ("Digite seu nome:")                                 
+/*Lista de Exercicios-Professor Adriano-Funções*/
+/*ATIVIDADE 1*/
+let contador ="";
+function piramideNumerica(){
+let numero = Number(prompt("Digite o número maior que zero(0):"));
+for (let i= 1;i<= numero;i++){
+ contador += i + " "; 
+    console.log(contador)
+}
+}
+piramideNumerica();
+
