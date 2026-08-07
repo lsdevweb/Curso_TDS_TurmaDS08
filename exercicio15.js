8c8284c7188d8918b8daa1138543c924b5ca4e56
Lucimar Santos-LS|Soluções <Lucimarcontas@Gmail.com>
2026-08-07 11:44:00 -0300

    Fix letter sequence logic in exercicio15.js

--- exercicio15.js
+++ exercicio15.js
@@ -1,21 +1,19 @@
 const prompt = require('prompt-sync')();
 /*sequencia de letras, de a ate a letra digitada;*/
 let contador = "";
-
 function sequenciaLetras() {
   let letra = prompt("Digite uma letra: ").toLowerCase();
   const alfabeto = "abcdefghijklmnopqrstuvwxyz";
 
   for (let i = 0; i < alfabeto.length; i++) {
-    let letraAtual = alfabeto[i];
-    contador += letraAtual + " ";
+    let letraDigitada = alfabeto[i];
+    contador += letraDigitada + " ";
     console.log(contador);
 
     // Se chegou na letra que o usuário digitou, o laço para
-    if (letraAtual === letra) {
+    if ( letraDigitada === letra) {
       break; 
     }
   }
 }
-
 sequenciaLetras();
